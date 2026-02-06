993db962467f4a61dd4b0a5c524f008649f1d96f
memoday <samyci@naver.com>
2026-02-07 01:52:39 +0900

    feat: 스택 선택 페이지 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Navigation } from "@/components/Navigation";
 
 
 export const metadata: Metadata = {
@@ -21,8 +22,14 @@ export default function RootLayout({
           crossOrigin="anonymous"
           href="https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css"
         />
+        <link 
+          rel="stylesheet" 
+          href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.7.2/css/all.min.css" 
+          crossOrigin="anonymous" 
+        />
       </head>
       <body>
+        <Navigation />
         {children}
       </body>
     </html>
